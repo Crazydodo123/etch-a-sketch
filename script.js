36bf01900adb3c5f14d8fd7d9e4355b2cf2cecdf
Crazydodo123 <peizheg@gmail.com>
2023-06-05 20:17:15 -0400

    add drawing functionality

--- script.js
+++ script.js
@@ -6,18 +6,31 @@ const row = document.createElement('div');
 const box = document.createElement('div');
 
 row.setAttribute('class', 'row');
-box.setAttribute('class', 'box')
+box.setAttribute('class', 'box');
 
 
+
+// Setting up functions
+
+function draw(element) {
+    element.classList.add('colored')
+}
 
 // Initializing
 
-for (let i = 1; i < 16; i++) {
+for (let i = 1; i <= 16; i++) {
     row.appendChild(box.cloneNode());
 }
 
-for (let i = 1; i < 16; i++) {
+for (let i = 1; i <= 16; i++) {
     container.appendChild(row.cloneNode(true));
 }
 
+const boxes = document.querySelectorAll('.box');
 
+boxes.forEach(box => {
+    box.addEventListener('mouseenter', () => {
+        console.log('hi')
+        draw(box);
+    });
+});
