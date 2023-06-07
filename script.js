baaa43ca16b7ed094e1475558ba82e4e7f0da14a
Crazydodo123 <peizheg@gmail.com>
2023-06-07 12:22:58 -0400

    add new buttons with functionality

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 // Defining objects
 
 const container = document.querySelector('#container');
+const controller = new AbortController;
+
+const rgbButton = document.querySelector('#RGB');
+const greyButton = document.querySelector('#greyscale')
+
+
 
 let row = document.createElement('div');
 let box = document.createElement('div');
+let rgb = false;
+let greyscale = false;
+let boxes;
 
 row.setAttribute('class', 'row');
 box.setAttribute('class', 'box');
@@ -13,7 +22,20 @@ box.setAttribute('class', 'box');
 // Setting up functions
 
 function draw(element) {
-    element.classList.add('colored')
+    if (rgb) {
+        let randomNum = Math.floor(Math.random() * 16777216)
+        let coloredNum = randomNum.toString(16).padStart(6, "0");
+        element.style.backgroundColor = `#${coloredNum}`
+
+    } else if (greyscale) {
+
+        greyDraw(element, element.style.backgroundColor)
+
+
+    } else {
+        element.style.backgroundColor = 'black';
+    }
+
 }
 
 
@@ -47,13 +69,12 @@ function createBox(number) {
 
 
 function setupBox(boxNum) {
-    const boxes = document.querySelectorAll('.box');
+    boxes = document.querySelectorAll('.box');
     
     boxes.forEach(box => {
         box.addEventListener('mouseenter', () => {
             draw(box);
-        });
-        
+        }, {signal: controller.signal});     
     });
 }
 
@@ -72,6 +93,67 @@ function updateCanvas() {
     createCanvas(number);
 }
 
+
 // Initializing
 
 createCanvas(16);
+
+
+// Fancy functions
+
+function reset() {
+    boxes.forEach(box => {
+        box.style.backgroundColor = 'transparent';
+    });
+}
+
+
+function setRGB() {
+    if (!rgb) {
+        rgb = true;
+    } else {
+        rgb = false;
+    }
+
+    if (greyscale) {
+        greyscale = false;
+        greyButton.classList.remove('greyOn');
+    }
+
+    rgbButton.classList.toggle('rgbOn');
+    
+}
+
+
+function setGrey() {
+    if (!greyscale) {
+        greyscale = true;
+    } else {
+        greyscale = false;
+    }
+
+    if (rgb) {
+        rgb = false;
+        rgbButton.classList.remove('rgbOn');
+    }
+
+    greyButton.classList.toggle('greyOn');
+}
+
+
+function greyDraw(element, currentColor) {
+    if (!currentColor || currentColor == 'transparent') {
+        console.log('hi');
+        element.style.backgroundColor = `rgb(90%, 90%, 90%)`;
+    } else if (currentColor == 'black') {
+        return false;
+    } else {
+        let colors = currentColor.split(',');
+        let redValue = colors[0].substring(4);
+        let greenValue = colors[1].substring(1);
+        let blueValue = colors[2].substring(1, colors[2].length - 1);
+
+        element.style.backgroundColor = `rgb(${redValue - 25.6}, ${greenValue - 25.6}, ${blueValue - 25.6})`;
+    }
+
+}
